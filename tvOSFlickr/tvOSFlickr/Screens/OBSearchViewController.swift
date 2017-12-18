66caf3e54e6f99bce37b4d71a13c4e2f3e069285
Oleksandr Borovok <borovok@online.ua>
2017-12-18 18:39:56 +0200

    OBSearchViewController: Actions

--- tvOSFlickr/tvOSFlickr/Screens/OBSearchViewController.swift
+++ tvOSFlickr/tvOSFlickr/Screens/OBSearchViewController.swift
@@ -13,11 +13,14 @@ class OBSearchViewController: UIViewController {
     @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet weak var searchButton: UIButton!
     
-    // MARK: -
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    // MARK: - Action
+    @IBAction func searchAction(_ sender: Any) {
+        
     }
+}
 
+extension OBSearchViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return textField.resignFirstResponder()
+    }
 }
