c685d7e6db09474054455cc4da16c76decb143ec
Oleksandr Borovok <borovok@online.ua>
2017-12-18 02:27:20 +0200

    Search View Controller : initial

---
+++ tvOSFlickr/tvOSFlickr/Screens/OBSearchViewController.swift
@@ -0,0 +1,23 @@
+//
+//  OBSearchViewController.swift
+//  tvOSFlickr
+//
+//  Created by Oleksandr Borovok on 12/18/17.
+//  Copyright © 2017 Oleksandr Borovok. All rights reserved.
+//
+
+import UIKit
+
+class OBSearchViewController: UIViewController {
+    // MARK: - Properties
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    
+    // MARK: -
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
